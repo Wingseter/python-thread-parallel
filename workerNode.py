e4331bd5dc89b5258f2f7aa4c545e8d9d80e48ec
kwon <wingseter@gmail.com>
2025-02-26 13:24:47 +0900

    [feat] make basic multithread

--- workerNode.py
+++ workerNode.py
@@ -1,6 +1,7 @@
 import pika
 import pymongo
 from concurrent.futures import ThreadPoolExecutor
+import threading
 
 # MongoDB 연결
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -24,8 +25,33 @@ def process_message(ch, method, properties, body):
     print(f"[Worker] Received: {message}")
 
     def task():
-        # TODO 멀티 프로세스 활동 하기
-        pass
+        # 메시지를 대문자로 변환하는 작업을 각 문자별로 스레드
+        def uppercase_char(char):
+            return char.upper()
+
+        def thread_task(char, result, index):
+            result[index] = uppercase_char(char)
+
+        result = [''] * len(message)
+        threads = []
+
+        # 각 문자를 대문자로 바꾸는 스레드 생성
+        for index, char in enumerate(message):
+            thread = threading.Thread(target=thread_task, args=(char, result, index))
+            threads.append(thread)
+            thread.start()
+
+
+        for thread in threads:
+            thread.join()
+
+        uppercased_message = ''.join(result)
+        print(f"Uppercased Message: {uppercased_message}")
+
+        collection.insert_one({"message": message})
+        print(f"Saved to MongoDB: {message}")
+    
+    task()
 
     # ACK 보내기
     ch.basic_ack(delivery_tag=method.delivery_tag)
