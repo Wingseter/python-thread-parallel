63c4ce5e6ab72ea7ef3ed13dcf18823d8d9e5750
kwon <wingseter@gmail.com>
2025-02-26 13:01:23 +0900

    [feat] worker and provider node

--- workerNode.py
+++ workerNode.py
@@ -1,33 +1,42 @@
 import pika
 import pymongo
+from concurrent.futures import ThreadPoolExecutor
 
 # MongoDB 연결
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = mongo_client["test_db"]
 collection = db["processed_tasks"]
 
-# RabbitMQ 연결
-credentials = pika.PlainCredentials('test', 'test')
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672, credentials=credentials))
-channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
+def create_channel():
+    credentials = pika.PlainCredentials('test', 'test')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672, credentials=credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
 
-# 메시지 처리 함수
-def callback(ch, method, properties, body):
+    channel.basic_qos(prefetch_count=1)
+
+    return connection, channel
+
+# 메시지 처리 함수 
+def process_message(ch, method, properties, body):
     message = body.decode()
-    print(f"Received {message}")
+    print(f"[Worker] Received: {message}")
 
-    # MongoDB에 저장
-    collection.insert_one({"message": message})
-    print(f"Saved to MongoDB: {message}")
+    def task():
+        # TODO 멀티 프로세스 활동 하기
+        pass
 
     # ACK 보내기
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-# 소비자 설정
-channel.basic_qos(prefetch_count=1)  # 한 번에 하나의 메시지만 처리
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
+# RabbitMQ Worker
+def worker():
+    connection, channel = create_channel()
+    channel.basic_consume(queue='task_queue', on_message_callback=process_message)
+    print(f"Worker started")
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    worker()
--- workerProducer.py
+++ workerProducer.py
@@ -1,4 +1,5 @@
 import pika
+import time
 
 # RabbitMQ 연결
 credentials = pika.PlainCredentials('test', 'test')
@@ -9,15 +10,23 @@ channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
 # 메시지 전송
-message = "Hello, workerNode!"
-channel.basic_publish(
-    exchange='',
-    routing_key='task_queue',
-    body=message,
-    properties=pika.BasicProperties(
-        delivery_mode=2,  
-    )
-)
+def send_messages(num_messages=10):
+    for i in range(1, num_messages + 1):
+        message = f"Task {i}"
+        
+        channel.basic_publish(
+            exchange='',
+            routing_key='task_queue',
+            body=message,
+            properties=pika.BasicProperties(
+                delivery_mode=2,  
+            )
+        )
+        
+        print(f"Sent: {message}")
+        time.sleep(0.5)  # TODO Delete Just for test
 
-print(f" [x] Sent {message}")
-connection.close()
+    print("모든 메시지 전송 완료")
+    connection.close()
+
+send_messages(num_messages=20)
