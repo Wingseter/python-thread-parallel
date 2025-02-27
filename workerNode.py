2c1518dc7c55952c330125fec3732954ac04c2dd
kwon <wingseter@gmail.com>
2025-02-27 19:17:00 +0900

    [feat] use db to dont duplicate

--- workerNode.py
+++ workerNode.py
@@ -16,49 +16,51 @@ def create_channel():
     credentials = pika.PlainCredentials('test', 'test')
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672, credentials=credentials))
     channel = connection.channel()
-
     # 큐 선언 시 TTL 설정 5초
     ttl = 5000  
-    arguments = {
-        'x-message-ttl': ttl  
-    }
+    arguments = { 'x-message-ttl': ttl  }
 
     channel.queue_declare(queue='task_queue', durable=True, arguments=arguments)
-
     channel.basic_qos(prefetch_count=1)
-
-
     return connection, channel
 
 def process_message(ch, method, properties, body):
     message_data = json.loads(body.decode())
-    status = message_data["status"]
+    task_id = message_data["task_id"]
     message = message_data["message"]
 
-    # 강제 오류 발생 테스트
+    if collection.find_one({"task_id": task_id}):  # 이미 처리된 메시지인 경우
+        print(f"[Worker] Task ID: {task_id} already processed")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        return
+
+    # 강제 오류 발생 테스트 TODO: 리모드로 변경
     error_num = random.randint(1, 20)
 
     try:
-        print(f"[Worker] Received: {message}, Status: {status}")
-        if error_num == 10: # 5% 확률
-            status = "Failed"
-        else:
-            status = "Success"
-
-        if status == "Failed":
-            print("--------Error 발생-------")  # Exception!
-            raise Exception("Intentional error for testing")
+        print(f"[Worker] Processing: {message}, Task ID: {task_id}")
+        if error_num == 1: # 5% 확률
+            print("--------Error 발생-------")  # Exception ACK 안보내고 Queue에 다시 넣기
+            raise Exception("에러상황1: 들어온 정보에 작업 중에 에러가 발생한 상황")
 
         def task():
+            result = [''] * len(message)
+            threads = []
+            error_flag = 1
+
+            # 예외 처리를 강화해서 각 
             # 메시지를 대문자로 변환하는 작업을 각 문자별로 쓰레드로 처리
             def uppercase_char(char):
                 return char.upper()
 
+            # 각 문자를 대문자로 변환하는 작업을 수행하는 스레드 함수
             def thread_task(char, result, index):
-                result[index] = uppercase_char(char)
+                try:
+                    result[index] = uppercase_char(char)
+                except Exception as e:
+                    print(f"Error in thread_task: {e}")
+                    error_flag = error_flag * 0
 
-            result = [''] * len(message)
-            threads = []
 
             # 각 문자를 대문자로 바꾸는 스레드 생성
             for index, char in enumerate(message):
@@ -70,21 +72,30 @@ def process_message(ch, method, properties, body):
             for thread in threads:
                 thread.join()
 
+            # 멀티 쓰레드 중 하나라도 에러가 발생할 경우
+            if error_flag == 0:
+                raise Exception("에러상황3: 강화된 멀티 쓰레드 에러 처리")
+
+            # 멀티 쓰레드 결과 출력
             uppercased_message = ''.join(result)
             print(f"Uppercased Message: {uppercased_message}")
 
             # MongoDB에 저장
-            collection.insert_one({"message": uppercased_message})
+            collection.insert_one({"task_id": task_id, "message": uppercased_message, "status": "Success"})
         
         task()
 
+        if error_num == 2: # 역시 5% 확률
+            print("--------Error 발생-------")  # Exception 완료 했는데 ACK 안보낼 경우
+            raise Exception("에러상황2: 작업 완료 했는데 노드가 불안정해서 중복이 발생할 수 있는 상황")
+ 
         # 정상적으로 메시지가 처리되었으므로 ACK 보내기
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     except Exception as e:
         print(f"[Worker] Error: {e}")
         
-        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)  # 재처리
+        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True) 
 
     time.sleep(0.5)  # TODO: Just for test, remove later
 
--- workerProducer.py
+++ workerProducer.py
@@ -2,6 +2,7 @@ import pika
 import time
 import random
 import json
+import uuid
 
 credentials = pika.PlainCredentials('test', 'test')
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672, credentials=credentials))
@@ -9,24 +10,19 @@ channel = connection.channel()
 
 # 큐 선언 시 TTL 설정 5초
 ttl = 5000  
-arguments = {
-    'x-message-ttl': ttl  
-}
-
+arguments = { 'x-message-ttl': ttl }
 channel.queue_declare(queue='task_queue', durable=True, arguments=arguments)
-
 channel.basic_qos(prefetch_count=1)
 
 
 # 메시지 전송
 def send_messages(num_messages=10):
-
-
     for i in range(1, num_messages + 1):
+        task_id = str(uuid.uuid4())
         message = f"Message {i}"
         status = "Not Processed"
 
-        message_body = json.dumps({"status": status, "message": message})
+        message_body = json.dumps({"task_id": task_id, "status": status, "message": message})
         
         try:
             channel.basic_publish(
